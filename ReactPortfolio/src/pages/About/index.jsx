7e7ffac20ea299d18635580faa88b733f7551898
tlesner <tjlesner@gmail.com>
2025-04-20 20:44:33 -0600

    adding pictures

--- ReactPortfolio/src/pages/About/index.jsx
+++ ReactPortfolio/src/pages/About/index.jsx
@@ -11,7 +11,7 @@ export default function About() {
                 src={Thomas}
                 alt="Picture of Thomas Lesner" 
             />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos rerum nostrum, dicta non fugit nam earum impedit dolores voluptatibus commodi, assumenda iure! Dolore molestiae soluta tempore reiciendis! Laboriosam, iure enim?</p>
+            <p>Im Thomas Lesner, an Account Executive and Manager with nearly a decade of leadership experience and a strong track record in driving results. With a customer-centric mindset and a passion for communication, Ive led high-performing teams and consistently broken sales records across both private and government sectors. Recently, Ive wanted to find a new challenge in life, a greater purpose. I hope to one day become a leader and mentor in the coding industry. Join me on my journey!</p>
         </div>
     )
 }
